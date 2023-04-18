8f3f9afacd2b55f9aee86b793716a850410a1c43
WaBiKong <ricardo2080355415@gmail.com>
2023-04-18 19:22:16 +0800

    update book case

--- 9/9-6mytalk_client.cpp
+++ 9/9-6mytalk_client.cpp
@@ -10,74 +10,74 @@
 #include <stdlib.h>
 #include <poll.h>
 #include <fcntl.h>
+#include <libgen.h>
 
 #define BUFFER_SIZE 64
 
-int main( int argc, char* argv[] )
-{
-    if( argc <= 2 )
-    {
-        printf( "usage: %s ip_address port_number\n", basename( argv[0] ) );
+// 一是从标准输入终端读入用户数据，并将用户数据发送至服务器
+// 二是往标准输出终端打印服务器发送给它的数据
+
+int main(int argc, char* argv[]) {
+    if (argc <= 2) {
+        printf("usage: %s ip_address port_number\n", basename(argv[0]));
         return 1;
     }
     const char* ip = argv[1];
-    int port = atoi( argv[2] );
+    int port = atoi(argv[2]);
 
     struct sockaddr_in server_address;
-    bzero( &server_address, sizeof( server_address ) );
+    bzero(&server_address, sizeof(server_address));
     server_address.sin_family = AF_INET;
-    inet_pton( AF_INET, ip, &server_address.sin_addr );
-    server_address.sin_port = htons( port );
+    inet_pton(AF_INET, ip, &server_address.sin_addr.s_addr);
+    server_address.sin_port = htons(port);
 
-    int sockfd = socket( PF_INET, SOCK_STREAM, 0 );
-    assert( sockfd >= 0 );
-    if ( connect( sockfd, ( struct sockaddr* )&server_address, sizeof( server_address ) ) < 0 )
-    {
-        printf( "connection failed\n" );
-        close( sockfd );
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sockfd >= 0);
+    if (connect(sockfd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        printf("connection failed\n");
+        close(sockfd);
         return 1;
     }
 
     pollfd fds[2];
-    fds[0].fd = 0;
+    fds[0].fd = 0; // 0号fd为标准输入
     fds[0].events = POLLIN;
     fds[0].revents = 0;
     fds[1].fd = sockfd;
     fds[1].events = POLLIN | POLLRDHUP;
     fds[1].revents = 0;
+
     char read_buf[BUFFER_SIZE];
     int pipefd[2];
-    int ret = pipe( pipefd );
-    assert( ret != -1 );
+    int ret = pipe(pipefd);
+    assert(ret != -1);
 
-    while( 1 )
-    {
-        ret = poll( fds, 2, -1 );
-        if( ret < 0 )
-        {
-            printf( "poll failure\n" );
+    while (1) {
+        ret = poll(fds, 2, -1);
+        if (ret < 0) {
+            printf("poll failure\n");
             break;
         }
 
-        if( fds[1].revents & POLLRDHUP )
-        {
-            printf( "server close the connection\n" );
+        if (fds[1].revents & POLLRDHUP) {
+            printf("server close the connection\n");
             break;
-        }
-        else if( fds[1].revents & POLLIN )
-        {
-            memset( read_buf, '\0', BUFFER_SIZE );
-            recv( fds[1].fd, read_buf, BUFFER_SIZE-1, 0 );
-            printf( "%s\n", read_buf );
+        } else if (fds[1].revents & POLLIN) { // 有socket消息发送过来
+            memset(read_buf, '\0', BUFFER_SIZE);
+            recv(fds[1].fd, read_buf, BUFFER_SIZE - 1, 0);
+            printf("%s\n", read_buf); // 将数据输出到标注输出终端
         }
 
-        if( fds[0].revents & POLLIN )
-        {
-            ret = splice( 0, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE );
-            ret = splice( pipefd[0], NULL, sockfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE );
+        // 此处实现了将标准输入的内容写入到用以socket通信的文件描述符中，完成发送
+        if (fds[0].revents & POLLIN) { // fds[0]的文件描述符为0，这表示有标准输入进来
+            // splice操作的两个文件描述符至少有一个得是管道
+            // 将标准输入零拷贝管道写端
+            ret = splice(0, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
+            // 将管道读端的数据零拷贝进sockfd进行通信
+            ret = splice(pipefd[0], NULL, sockfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
         }
     }
-    
-    close( sockfd );
+
+    close(sockfd);
     return 0;
 }
